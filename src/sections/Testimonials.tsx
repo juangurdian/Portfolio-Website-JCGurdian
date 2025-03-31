66ec67e098133a52b36d177dfd665b2f72ecda90
juangurdian <juangurdian2003@gmail.com>
2025-03-31 14:57:04 -0500

    testimonials responsiveness

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -43,7 +43,7 @@ const testimonials = [
 
 export const TestimonialsSection = () => {
   return (
-  <div className="py-16">
+  <div className="py-16 lg:py-24">
     <div className="container">
       <SectionHeader 
         title={"What Clients Say About Me"} 
@@ -51,10 +51,10 @@ export const TestimonialsSection = () => {
         description={"See what clients sau about my work"} />
 
 
-      <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+      <div className="mt-16 lg:mt-24 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
         <div className="flex flex-none gap-8">
         {testimonials.map(testimonial => (
-          <Card key={testimonial.name} className="max-w-xs">
+          <Card key={testimonial.name} className="max-w-xs md:max-w-md md:p-8">
             <div className="flex gap-4 items-center">
               <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
             <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
@@ -64,7 +64,7 @@ export const TestimonialsSection = () => {
                 <div className="text-white/40 text-sm">{testimonial.position}</div>
               </div>
             </div>
-            <p className="text-white mt-4 text-sm">{testimonial.text}</p>
+            <p className="text-white mt-4 md:mt-6 text-sm md:text-base">{testimonial.text}</p>
           </Card>
         ))}
       </div>
