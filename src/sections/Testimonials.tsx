5776d6e18826539578904f4a39d633bdd4d0f281
juangurdian <juangurdian2003@gmail.com>
2025-03-31 14:53:53 -0500

    testimonials section

--- src/sections/Testimonials.tsx
+++ src/sections/Testimonials.tsx
@@ -1,8 +1,12 @@
+import Image from "next/image";
 import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
 import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
 import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
 import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
 import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import { SectionHeader } from "@/components/Sectionheader";
+import grainImage from "@/assets/images/grain.jpg";
+import { Card } from "@/components/Card";
 
 const testimonials = [
   {
@@ -38,5 +42,34 @@ const testimonials = [
 ];
 
 export const TestimonialsSection = () => {
-  return <div>Testimonials Section</div>;
+  return (
+  <div className="py-16">
+    <div className="container">
+      <SectionHeader 
+        title={"What Clients Say About Me"} 
+        eyebrow={"Happy Clients"} 
+        description={"See what clients sau about my work"} />
+
+
+      <div className="mt-16 flex overflow-x-clip [mask-image:linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="flex flex-none gap-8">
+        {testimonials.map(testimonial => (
+          <Card key={testimonial.name} className="max-w-xs">
+            <div className="flex gap-4 items-center">
+              <div className="size-14 bg-gray-700 inline-flex items-center justify-center rounded-full flex-shrink-0">
+            <Image src={testimonial.avatar} alt={testimonial.name} className="max-h-full" />
+              </div>
+              <div>
+                <div className="text-white font-semibold">{testimonial.name}</div>
+                <div className="text-white/40 text-sm">{testimonial.position}</div>
+              </div>
+            </div>
+            <p className="text-white mt-4 text-sm">{testimonial.text}</p>
+          </Card>
+        ))}
+      </div>
+    </div>
+    </div>
+  </div>
+  );
 };
