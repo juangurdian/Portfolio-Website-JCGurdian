e64d6df2e17c7ffcb14ff92c99741a8d43953080
juangurdian <juangurdian2003@gmail.com>
2025-03-31 04:11:48 -0500

    project cards responsiveness

--- src/sections/Projects.tsx
+++ src/sections/Projects.tsx
@@ -51,26 +51,26 @@ export const ProjectsSection = () => {
     throw new Error("Function not implemented.");
   }
 
-  return <section className="pb-16">
+  return <section className="pb-16 lg:py-24">
     <div className="container text-white">
       <div className="flex justify-center">
         <p className="uppercase font-semibold tracking-widest bg-gradient-to-r from-emerald-300 to-sky-400 bg-clip-text text-transparent">Real-world Results</p>
       </div>
       <h2 className="font-serif text-3xl md:text-5xl text-center mt-6">Featured Projects</h2>
-      <p className="text-center md:text-lg text-white/60 mt-4 max-w-md mx-auto">Projects</p>
+      <p className="text-center md:text-lg lg:text-xl text-white/60 mt-4 max-w-md mx-auto">Projects</p>
       <div className="mt-10 md:mt-20 flex flex-col gap-20">
         {portfolioProjects.map(project => (
           <div key={project.title} className="bg-gray-800 rounded-3xl relative z-0
            overflow-hidden after:-z-10 after:content-[''] after:absolute 
            after:inset-0 after:outline-2 after:outline after:-outline-offset-2 
-           after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none">
+           after:rounded-3xl after:outline-white/20 px-8 pt-8 md:pt-12 md:px-10 after:pointer-events-none lg:pt-16 lg:px-20">
             <div className="absolute inset-0 -z-10 opacity-5" style={{
               backgroundImage: `url(${grainImage.src})`,
             }}>
 
             </div>
-
-
+            <div className="lg:grid lg:grid-cols-2 lg:gap-16">
+              <div className="lg:pb-16">
               <div className="bg-gradient-to-r from-emerald-300 to-sky-400 
               inline-flex gap-2 font-bold uppercase tracking-widest text-sm text-transparent bg-clip-text">
 
@@ -97,7 +97,11 @@ export const ProjectsSection = () => {
                 <ArrowUpRightIcon classNAme="size-4"/>
               </button>
             </a>
-            <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0" />
+            </div>
+            <div className="relative">
+            <Image src={project.image} alt={project.title} className="mt-8 -mb-4 md:-mb-0 lg:mt-0 lg:absolute lg:h-full lg:w-auto lg:max-w-none" />
+          </div>
+          </div>
           </div>
         ))}
       </div>
