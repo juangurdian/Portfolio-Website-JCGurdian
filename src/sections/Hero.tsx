45a6491d47b5f98893559107645d5daba7afe5b7
juangurdian <juangurdian2003@gmail.com>
2025-04-10 14:44:28 -0500

    fix bugs

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -45,7 +45,7 @@ export const HeroSection = () => {
             href="#contact"
             className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl hover:bg-gray-100 transition-colors"
           >
-            <span className="font-semibold">Let's Connect</span>
+            <span className="font-semibold">Let&apos;s Connect</span>
             <ArrowUpRightIcon className="size-4" />
           </a>
         </div>
